b8822d06729c39fa145c9decd0eaefa819cf75ca
Bosco Ho <apposite@gmail.com>
2020-08-15 09:56:47 +1000

    renamed example file, .gitignore

---
+++ go.py
@@ -0,0 +1,16 @@
+import sys
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+from modeldrop.app import DashModelAdaptor, open_url_in_background
+from modeldrop.goodwin import GoodwinModel
+from modeldrop.keen import KeenModel
+from modeldrop.property import PropertyModel
+from modeldrop.turchin import DemographicFiscalModel
+
+port = "8050"
+if "-o" in sys.argv:
+    open_url_in_background(f"http://127.0.0.1:{port}/")
+
+models = [KeenModel(), DemographicFiscalModel(), PropertyModel(), GoodwinModel()]
+DashModelAdaptor(models).run_server(port=port)
